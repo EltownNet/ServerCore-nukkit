77c03a5c8ecfd9ea3b679542ceac0dd3519666b1
Mundschutziii <37897528+Mundschutziii@users.noreply.github.com>
2021-08-04 06:30:51 +0200

    added bank services

--- src/main/java/net/eltown/servercore/components/roleplay/jobs/BankRoleplay.java
+++ src/main/java/net/eltown/servercore/components/roleplay/jobs/BankRoleplay.java
@@ -38,28 +38,29 @@ public class BankRoleplay {
         if (item.getId() == ItemID.PAPER && item.getNamedTag().getString("bank_account") != null) {
             final String account = item.getNamedTag().getString("bank_account");
 
-            final BankAccount bankAccount = Economy.getBankAPI().getAccount(account);
-            if (bankAccount != null) {
-                final CustomForm form = new CustomForm.Builder("§7» §8Bankkonto-Login")
-                        .addElement(new ElementLabel("§fKonto: §9" + account))
-                        .addElement(new ElementInput("§fBitte gebe das Passwort des Kontos an.", "Passwort"))
-                        .onSubmit((g, h) -> {
-                            final String password = h.getInputResponse(1);
+            Economy.getBankAPI().getAccount(account, bankAccount -> {
+                if (bankAccount != null) {
+                    final CustomForm form = new CustomForm.Builder("§7» §8Bankkonto-Login")
+                            .addElement(new ElementLabel("§fKonto: §9" + account))
+                            .addElement(new ElementInput("§fBitte gebe das Passwort des Kontos an.", "Passwort"))
+                            .onSubmit((g, h) -> {
+                                final String password = h.getInputResponse(1);
 
-                            if (password.equals(bankAccount.getPassword())) {
-                                this.openBankAccount(player, bankAccount.getAccount());
-                                this.serverCore.playSound(player, Sound.NOTE_PLING);
-                            } else {
-                                player.sendMessage(Language.get("roleplay.bank.invalid.password"));
-                                this.serverCore.playSound(player, Sound.NOTE_BASS);
-                            }
-                        })
-                        .build();
-                form.send(player);
-            } else {
-                player.sendMessage(Language.get("roleplay.bank.invalid.account"));
-                this.serverCore.playSound(player, Sound.NOTE_BASS);
-            }
+                                if (password.equals(bankAccount.getPassword())) {
+                                    this.openBankAccount(player, bankAccount.getAccount());
+                                    this.serverCore.playSound(player, Sound.NOTE_PLING);
+                                } else {
+                                    player.sendMessage(Language.get("roleplay.bank.invalid.password"));
+                                    this.serverCore.playSound(player, Sound.NOTE_BASS);
+                                }
+                            })
+                            .build();
+                    form.send(player);
+                } else {
+                    player.sendMessage(Language.get("roleplay.bank.invalid.account"));
+                    this.serverCore.playSound(player, Sound.NOTE_BASS);
+                }
+            });
         } else {
             player.sendMessage(Language.get("roleplay.bank.card.needed"));
             this.serverCore.playSound(player, Sound.NOTE_BASS);
@@ -67,75 +68,78 @@ public class BankRoleplay {
     }
 
     public void openBankAccount(final Player player, final String account) {
-        final BankAccount bankAccount = Economy.getBankAPI().getAccount(account);
-        final SimpleForm form = new SimpleForm.Builder("§7» §8Bankkonto", "§fKonto: §9" + bankAccount.getAccount() + "\n§fInhaber: §9" + bankAccount.getOwner() + "\n\n§fGuthaben: §a$" + Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance()) + "\n")
-                .addButton(new ElementButton("§8» §3Guthaben einzahlen"), e -> this.openDepositMenu(e, bankAccount.getAccount()))
-                .addButton(new ElementButton("§8» §3Guthaben abheben"), e -> this.openWithdrawMenu(e, bankAccount.getAccount()))
-                .build();
-        form.send(player);
+        Economy.getBankAPI().getAccount(account, bankAccount -> {
+            final SimpleForm form = new SimpleForm.Builder("§7» §8Bankkonto", "§fKonto: §9" + bankAccount.getAccount() + "\n§fInhaber: §9" + bankAccount.getOwner() + "\n\n§fGuthaben: §a$" + Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance()) + "\n")
+                    .addButton(new ElementButton("§8» §3Guthaben einzahlen"), e -> this.openDepositMenu(e, bankAccount.getAccount()))
+                    .addButton(new ElementButton("§8» §3Guthaben abheben"), e -> this.openWithdrawMenu(e, bankAccount.getAccount()))
+                    .build();
+            form.send(player);
+        });
     }
 
     public void openDepositMenu(final Player player, final String account) {
-        final BankAccount bankAccount = Economy.getBankAPI().getAccount(account);
+        Economy.getBankAPI().getAccount(account, bankAccount -> {
+            final CustomForm form = new CustomForm.Builder("§7» §8Guthaben einzahlen")
+                    .addElement(new ElementLabel("§fKonto: §9" + bankAccount.getAccount() + "\n§fInhaber: §9" + bankAccount.getOwner() + "\n\n§fGuthaben: §a$" + Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance()) + "\n"))
+                    .addElement(new ElementInput("Bitte gebe an, wie viel Geld du auf dieses Konto einzahlen möchtest.", "3.99"))
+                    .onSubmit((g, h) -> {
+                        try {
+                            final double amount = Double.parseDouble(h.getInputResponse(1));
+                            if (amount <= 0) throw new Exception("Invalid bank interact amount");
 
-        final CustomForm form = new CustomForm.Builder("§7» §8Guthaben einzahlen")
-                .addElement(new ElementLabel("§fKonto: §9" + bankAccount.getAccount() + "\n§fInhaber: §9" + bankAccount.getOwner() + "\n\n§fGuthaben: §a$" + Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance()) + "\n"))
-                .addElement(new ElementInput("Bitte gebe an, wie viel Geld du auf dieses Konto einzahlen möchtest.", "3.99"))
-                .onSubmit((g, h) -> {
-                    try {
-                        final double amount = Double.parseDouble(h.getInputResponse(1));
-                        if (amount <= 0) throw new Exception("Invalid bank interact amount");
-
-                        Economy.getAPI().getMoney(player, money -> {
-                            if (money >= amount) {
-                                Economy.getAPI().reduceMoney(player, amount);
-                                Economy.getBankAPI().depositMoney(account, amount);
-                                player.sendMessage(Language.get("roleplay.bank.deposit.success", Economy.getAPI().getMoneyFormat().format(amount), Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance() + amount)));
-                                this.serverCore.playSound(player, Sound.NOTE_PLING);
-                            } else {
-                                player.sendMessage(Language.get("roleplay.bank.no.money"));
-                                this.serverCore.playSound(player, Sound.NOTE_BASS);
-                            }
-                        });
-                    } catch (final Exception e) {
-                        player.sendMessage(Language.get("roleplay.bank.invalid.input"));
-                        this.serverCore.playSound(player, Sound.NOTE_BASS);
-                    }
-                })
-                .build();
-        form.send(player);
+                            Economy.getAPI().getMoney(player, money -> {
+                                if (money >= amount) {
+                                    Economy.getAPI().reduceMoney(player, amount);
+                                    Economy.getBankAPI().depositMoney(account, amount);
+                                    Economy.getBankAPI().insertBankLog(account, "Geld eingezahlt", "§9" + player.getName() + " §fhat §a$" + amount + " §feingezahlt.");
+                                    player.sendMessage(Language.get("roleplay.bank.deposit.success", Economy.getAPI().getMoneyFormat().format(amount), Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance() + amount)));
+                                    this.serverCore.playSound(player, Sound.NOTE_PLING);
+                                } else {
+                                    player.sendMessage(Language.get("roleplay.bank.no.money"));
+                                    this.serverCore.playSound(player, Sound.NOTE_BASS);
+                                }
+                            });
+                        } catch (final Exception e) {
+                            player.sendMessage(Language.get("roleplay.bank.invalid.input"));
+                            this.serverCore.playSound(player, Sound.NOTE_BASS);
+                        }
+                    })
+                    .build();
+            form.send(player);
+        });
     }
 
     public void openWithdrawMenu(final Player player, final String account) {
-        final BankAccount bankAccount = Economy.getBankAPI().getAccount(account);
+        Economy.getBankAPI().getAccount(account, bankAccount -> {
+            final CustomForm form = new CustomForm.Builder("§7» §8Guthaben abheben")
+                    .addElement(new ElementLabel("§fKonto: §9" + bankAccount.getAccount() + "\n§fInhaber: §9" + bankAccount.getOwner() + "\n\n§fGuthaben: §a$" + Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance()) + "\n"))
+                    .addElement(new ElementInput("Bitte gebe an, wie viel Geld du von diesem Konto abheben möchtest.", "3.99"))
+                    .onSubmit((g, h) -> {
+                        try {
+                            final double amount = Double.parseDouble(h.getInputResponse(1));
+                            if (amount <= 0) throw new Exception("Invalid bank interact amount");
 
-        final CustomForm form = new CustomForm.Builder("§7» §8Guthaben abheben")
-                .addElement(new ElementLabel("§fKonto: §9" + bankAccount.getAccount() + "\n§fInhaber: §9" + bankAccount.getOwner() + "\n\n§fGuthaben: §a$" + Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance()) + "\n"))
-                .addElement(new ElementInput("Bitte gebe an, wie viel Geld du von diesem Konto abheben möchtest.", "3.99"))
-                .onSubmit((g, h) -> {
-                    try {
-                        final double amount = Double.parseDouble(h.getInputResponse(1));
-                        if (amount <= 0) throw new Exception("Invalid bank interact amount");
-
-                        if (bankAccount.getBalance() >= amount) {
-                            Economy.getAPI().addMoney(player, amount);
-                            Economy.getBankAPI().withdrawMoney(account, amount);
-                            player.sendMessage(Language.get("roleplay.bank.withdraw.success", Economy.getAPI().getMoneyFormat().format(amount), Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance() - amount)));
-                            this.serverCore.playSound(player, Sound.NOTE_PLING);
+                            if (bankAccount.getBalance() >= amount) {
+                                Economy.getAPI().addMoney(player, amount);
+                                Economy.getBankAPI().withdrawMoney(account, amount);
+                                Economy.getBankAPI().insertBankLog(account, "Geld abgehoben", "§9" + player.getName() + " §fhat §a$" + amount + " §fabgehoben.");
+                                player.sendMessage(Language.get("roleplay.bank.withdraw.success", Economy.getAPI().getMoneyFormat().format(amount), Economy.getAPI().getMoneyFormat().format(bankAccount.getBalance() - amount)));
+                                this.serverCore.playSound(player, Sound.NOTE_PLING);
+                            }
+                        } catch (final Exception e) {
+                            player.sendMessage(Language.get("roleplay.bank.invalid.input"));
+                            this.serverCore.playSound(player, Sound.NOTE_BASS);
                         }
-                    } catch (final Exception e) {
-                        player.sendMessage(Language.get("roleplay.bank.invalid.input"));
-                        this.serverCore.playSound(player, Sound.NOTE_BASS);
-                    }
-                })
-                .build();
-        form.send(player);
+                    })
+                    .build();
+            form.send(player);
+        });
     }
 
     public void openBankManager(final Player player) {
         final SimpleForm form = new SimpleForm.Builder("§7» §8Banker Chris", "")
                 .addButton(new ElementButton("§8» §fNeues Konto eröffnen"), this::openCreateBankAccount)
-                .addButton(new ElementButton("§8» §fBankkonto Beratung"), e -> {})
+                .addButton(new ElementButton("§8» §fBankkonto Beratung"), this::openBankManagerService)
                 .build();
         form.send(player);
     }
@@ -159,6 +163,76 @@ public class BankRoleplay {
                 .onNo(this::openBankManager)
                 .build();
         form.send(player);
+    }
+
+    public void openBankManagerService(final Player player) {
+        final Item item = player.getInventory().getItemInHand();
+        if (item.getId() == ItemID.PAPER && item.getNamedTag().getString("bank_account") != null) {
+            final String account = item.getNamedTag().getString("bank_account");
+
+            Economy.getBankAPI().getAccount(account, bankAccount -> {
+                if (bankAccount != null) {
+                    if (bankAccount.getOwner().equals(player.getName())) {
+                        final SimpleForm select = new SimpleForm.Builder("§7» §8Service wählen", "Bitte suche dir eines der folgenden Aktionen aus, um fortzufahren.\n\n§fKonto: §9" + bankAccount.getAccount())
+                                .addButton(new ElementButton("§8» §9Kontoaktivität"), e -> this.openBankManagerAccountLog(player, bankAccount))
+                                .addButton(new ElementButton("§8» §9Bankkarte"), e -> this.openBankManagerAccountCard(player, bankAccount))
+                                .addButton(new ElementButton("§8» §9Einstellungen"), e -> this.openBankManagerAccountSettings(player, bankAccount))
+                                .build();
+                        select.send(player);
+                    } else {
+                        player.sendMessage(Language.get("roleplay.bank.invalid.user"));
+                        this.serverCore.playSound(player, Sound.NOTE_BASS);
+                    }
+                } else {
+                    player.sendMessage(Language.get("roleplay.bank.invalid.account"));
+                    this.serverCore.playSound(player, Sound.NOTE_BASS);
+                }
+            });
+        } else {
+            player.sendMessage(Language.get("roleplay.bank.card.needed"));
+            this.serverCore.playSound(player, Sound.NOTE_BASS);
+        }
+    }
+
+    public void openBankManagerAccountLog(final Player player, final BankAccount bankAccount) {
+        final SimpleForm.Builder logs = new SimpleForm.Builder("§7» §8Kontoaktivität", "Konto: §9" + bankAccount.getAccount());
+
+        bankAccount.getBankLogs().forEach(e -> {
+            logs.addButton(new ElementButton(e.getTitle() + "\n§7" + e.getDate()), g -> {
+                final SimpleForm details = new SimpleForm.Builder("§7» §8Kontoaktivität", "Konto: §9" + bankAccount.getAccount() + "\n\n" +
+                        "§fLogID: §1" + e.getLogId() + "\n" + "§fAktion: §1" + e.getTitle() + "\n" + "§fBeschreibung: \n§1" + e.getDetails() + "\n" + "§fDatum: §1" + e.getDate() + "\n\n")
+                        .addButton(new ElementButton("§8» §cZurück"), h -> this.openBankManagerAccountLog(player, bankAccount))
+                        .build();
+                details.send(g);
+            });
+        });
+
+        logs.addButton(new ElementButton("§8» §cZurück"), this::openBankManagerService);
+
+        logs.build().send(player);
+    }
+
+    public void openBankManagerAccountCard(final Player player, final BankAccount bankAccount) {
+        final ModalForm form = new ModalForm.Builder("§7» §8Neue Bankkarte", "§fMöchtest du für das Konto §9" + bankAccount.getAccount() + " §feine neue Bankkarte beanspruchen?" +
+                "\n\n§fVerwaltungsgebühr: §a$20\n§fBankkarte: §a$35\n\n§f§lZu zahlen: §r§a$55", "§8» §aBeanspruchen", "§8» §cZurück")
+                .onYes(e -> Economy.getAPI().getMoney(player, money -> {
+                    if (money >= 55) {
+                        Economy.getAPI().reduceMoney(player, 55);
+                        this.giveBankCard(player, bankAccount.getAccount());
+                        player.sendMessage(Language.get("roleplay.bank.card.received"));
+                        this.serverCore.playSound(player, Sound.NOTE_PLING);
+                    } else {
+                        player.sendMessage(Language.get("roleplay.bank.no.money"));
+                        this.serverCore.playSound(player, Sound.NOTE_BASS);
+                    }
+                }))
+                .onNo(this::openBankManagerService)
+                .build();
+        form.send(player);
+    }
+
+    public void openBankManagerAccountSettings(final Player player, final BankAccount bankAccount) {
+
     }
 
     public void giveBankCard(final Player player, final String account) {
